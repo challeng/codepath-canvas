da00767753ef0721e6e7377d4374c913a606e766
Jesse Pinho <jesse@jessepinho.com>
2015-11-12 17:00:01 -0600

    Milestone 2

--- codepath-canvas/ViewController.m
+++ codepath-canvas/ViewController.m
@@ -9,7 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *trayView;
+@property (nonatomic, assign) CGPoint trayOriginalCenter;
 
+- (IBAction)onTrayPanGesture:(UIPanGestureRecognizer *)sender;
 @end
 
 @implementation ViewController
@@ -24,4 +27,19 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)onTrayPanGesture:(UIPanGestureRecognizer *)sender {
+    // Absolute (x,y) coordinates in parentView
+    CGPoint translation = [sender translationInView:self.trayView];
+    
+    if (sender.state == UIGestureRecognizerStateBegan) {
+        self.trayOriginalCenter = self.trayView.center;
+        NSLog(@"Gesture began at: %@", NSStringFromCGPoint(translation));
+    } else if (sender.state == UIGestureRecognizerStateChanged) {
+        NSLog(@"Gesture changed at: %@", NSStringFromCGPoint(translation));
+        self.trayView.center = CGPointMake(self.trayOriginalCenter.x,
+                                           self.trayOriginalCenter.y + translation.y);
+    } else if (sender.state == UIGestureRecognizerStateEnded) {
+        NSLog(@"Gesture ended at: %@", NSStringFromCGPoint(translation));
+    }
+}
 @end
